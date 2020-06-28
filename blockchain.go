ec2ab7ddfc25bb648c24543701d0e83c676e402a
tomonakar <tomohisa.nak@gmail.com>
2020-06-28 23:02:34 +0900

    feat: トランザクションの合計を算出するメソッドを追加

--- blockchain.go
+++ blockchain.go
@@ -141,6 +141,23 @@ func (bc *Blockchain) Mining() bool {
 	return true
 }
 
+func (bc *Blockchain) CaluculateTotalAmount(blockchainAddress string) float32 {
+	var totalAmount float32 = 0.0
+	for _, b := range bc.chain {
+		for _, t := range b.transactions {
+			value := t.value
+			if blockchainAddress == t.recipientBlockchainAddress {
+				totalAmount += value
+			}
+
+			if blockchainAddress == t.senderBlockchainAddress {
+				totalAmount -= value
+			}
+		}
+	}
+	return totalAmount
+}
+
 type Transaction struct {
 	senderBlockchainAddress    string
 	recipientBlockchainAddress string
@@ -197,4 +214,7 @@ func main() {
 	blockChain.Mining()
 	blockChain.Print()
 
+	fmt.Printf("my %.1f\n", blockChain.CaluculateTotalAmount("my_blockchain_address"))
+	fmt.Printf("C %.1f\n", blockChain.CaluculateTotalAmount("C"))
+	fmt.Printf("D %.1f\n", blockChain.CaluculateTotalAmount("D"))
 }
