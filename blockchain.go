719ad3ed69bc01e141ea079b6185eba8d391da61
tomonakar <tomohisa.nak@gmail.com>
2020-06-28 15:10:19 +0900

    feat: blockchainのstructを作成

--- blockchain.go
+++ blockchain.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 	"time"
 )
 
+// Blockの構造体
 type Block struct {
 	nonce        int
 	previousHash string
@@ -13,7 +15,8 @@ type Block struct {
 	transactions []string
 }
 
-func NewBlack(nonce int, previousHash string) *Block {
+// Blockを新規作成
+func NewBlock(nonce int, previousHash string) *Block {
 	// こう書いても良いし
 	b := new(Block) // newを使うとポインタが返る
 	b.timestamp = time.Now().UnixNano()
@@ -27,7 +30,7 @@ func NewBlack(nonce int, previousHash string) *Block {
 	// }
 }
 
-// Printメソッドは、BlockStructの中身を単に見やすくするためのもの
+// Block Structの中身を単に見やすくするためのもの
 func (b *Block) Print() {
 	fmt.Printf("timestamp          %d\n", b.timestamp)
 	fmt.Printf("nonce              %d\n", b.nonce)
@@ -35,12 +38,42 @@ func (b *Block) Print() {
 	fmt.Printf("transactions       %s\n", b.transactions)
 }
 
+// Blockchain構造体
+type Blockchain struct {
+	transactionPool []string
+	chain           []*Block
+}
+
+func NewBlockchain() *Blockchain {
+	bc := new(Blockchain)
+	bc.CreateBlock(0, "Init hash")
+	return bc
+}
+
+func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
+	b := NewBlock(nonce, previousHash)
+	bc.chain = append(bc.chain, b)
+	return b
+}
+
+func (bc *Blockchain) Print() {
+	for i, block := range bc.chain {
+		fmt.Printf("%s Chain %d %s\n", strings.Repeat("=", 25), i,
+			strings.Repeat("=", 25))
+		block.Print()
+	}
+	fmt.Printf("%s\n", strings.Repeat("*", 25))
+}
+
 func init() {
 	log.SetPrefix("Blockchain: ")
 }
+
 func main() {
-	log.Println("test")
-	fmt.Println("test2")
-	b := NewBlack(0, "init hash")
-	b.Print()
+	blockChain := NewBlockchain()
+	blockChain.Print()
+	blockChain.CreateBlock(5, "hash1")
+	blockChain.Print()
+	blockChain.CreateBlock(2, "hash2")
+	blockChain.Print()
 }
