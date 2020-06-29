f2c5a0fdec54ce2d00a9488ed9900ee36b7079fa
tomonakar <tomohisa.nak@gmail.com>
2020-06-29 09:06:25 +0900

    feat: walletの作成

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"goblockchain/block"
+	"goblockchain/wallet"
 	"log"
 )
 
@@ -11,20 +11,7 @@ func init() {
 }
 
 func main() {
-	myBlockchainAddress := "my_blockchain_address"
-	blockChain := block.NewBlockchain(myBlockchainAddress)
-	blockChain.Print()
-
-	blockChain.AddTransaction("A", "B", 1.0)
-	blockChain.Mining()
-	blockChain.Print()
-
-	blockChain.AddTransaction("C", "D", 2.0)
-	blockChain.AddTransaction("X", "Y", 3.0)
-	blockChain.Mining()
-	blockChain.Print()
-
-	fmt.Printf("my %.1f\n", blockChain.CaluculateTotalAmount("my_blockchain_address"))
-	fmt.Printf("C %.1f\n", blockChain.CaluculateTotalAmount("C"))
-	fmt.Printf("D %.1f\n", blockChain.CaluculateTotalAmount("D"))
+	w := wallet.NewWallet()
+	fmt.Println(w.PrivateKeyStr())
+	fmt.Println(w.PublicKeyStr())
 }
