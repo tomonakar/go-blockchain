73def2394e7d8fdda829c9f08d0b736dd1b0959d
tomonakar <tomohisa.nak@gmail.com>
2020-07-19 16:41:57 +0900

    feat: コンセンサスAPIを追加

--- block/blockchain.go
+++ block/blockchain.go
@@ -148,6 +148,7 @@ func (bc *Blockchain) SetNeighbors() {
 }
 func (bc *Blockchain) Run() {
 	bc.StartSyncNeighbors()
+	bc.ResolveConflicts()
 }
 
 func (bc *Blockchain) SyncNeighbors() {
@@ -305,6 +306,14 @@ func (bc *Blockchain) Mining() bool {
 	previousHash := bc.LastBlock().Hash()
 	bc.CreateBlock(nonce, previousHash)
 	log.Println("action=mining, status=success")
+
+	for _, n := range bc.neighbors {
+		endpoint := fmt.Sprintf("http://%s/consensus", n)
+		client := &http.Client{}
+		req, _ := http.NewRequest("PUT", endpoint, nil)
+		resp, _ := client.Do(req)
+		log.Printf("%v", resp)
+	}
 	return true
 }
 
